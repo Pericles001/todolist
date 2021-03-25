638571a104517e05d81e8124c9525d5e634dcbee
Pericles001 <periclesadjovi@utlook.fr>
2021-03-25 13:16:04 +0100

    rendering list

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+{/*<li class="collection-item">
+<div>
+  Buying Food<a href="#!" class="modal-trigger secondary-content"
+    ><i class="material-icons">delete</i></a
+  ><a href="#modal1" class="modal-trigger secondary-content"
+    ><i class="material-icons">edit</i></a
+  >
+</div>
+</li>*/}
+
+
+
+
+
+function renderList(doc){
+let li = document.createElement('li');
+li.className = "collection-item";
+li.setAttribute('data-id', doc.id)
+}
+
+
+
 db.collection('todos').orderBy('title').onSnapshot(snapshot => {
     let changes = snapshot.docChanges()
     //console.log(changes)
